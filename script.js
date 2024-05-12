8e44e45fe3eeddd2ec0b31b7f3691b9dba4aa90a
deepdev79 <deepankar7493@gmail.com>
2024-05-12 15:47:13 +0530

    error handling final

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const input = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-button");
 const pokemonImg = document.getElementById("poke-img");
